cf387714f5b423cae331bc17107c46a8a6ab12d4
peterrhysthomas <peterrhysthomas@yahoo.co.uk>
2021-11-15 15:49:49 +0000

    Update main.py

--- .github/actions/cat-facts/src/main.py
+++ .github/actions/cat-facts/src/main.py
@@ -7,7 +7,8 @@ cat_url = "https://cat-fact.herokuapp.com/facts"
 r = requests.get(cat_url)
 r_obj_list = r.json()
 
-print("results: " + r_obj_list)
+print("results: ")
+print(r_obj_list)
 
 # Create an empty list to store individual facts in
 # This will make it easy to select a random one later
